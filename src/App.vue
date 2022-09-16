de70179c1254803e1f78b5716f77ff953abd6bd7
Bilal Khan <bilal.khan@qordata.com>
2022-09-16 13:08:02 +0500

    add router

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,29 @@
 <template>
   <v-app>
     <v-main>
-        <HelloWorld v-if="is_authenticated"/>
-        <LogIn v-if="!is_authenticated" v-on:isAuthenticated="validation"/>
+        <!--<HelloWorld v-if="is_authenticated"/>-->
+        <!--<LogIn v-if="!is_authenticated" v-on:isAuthenticated="validation"/>-->
+        <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
-    import HelloWorld from './components/HelloWorld';
-    import LogIn from './components/LogIn';
 
 export default {
     name: 'App',
 
-    components: {
-        HelloWorld,
-        LogIn
-    },
-
     data: () => ({
         is_authenticated: false
     }),
+
     methods: {
-        validation(value) {
-            this.is_authenticated = value;
-        }
+        //validation(value) {
+        //    this.is_authenticated = value;
+        //}
     }
 };
+
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import VueRouter from 'vue-router'
+import LogIn from './components/LogIn.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const routes = [
+    { path: '/', redirect: '/login' }, // also can use alias: for this purpose, but that does not change the url
+    { path: '/login', component: LogIn },
+    { path: '/hello', component: HelloWorld }
+]
+
+const router = new VueRouter({
+    routes
+});
 
 new Vue({
-  vuetify,
-  render: h => h(App)
+    vuetify,
+    router,
+    render: h => h(App)
 }).$mount('#app')
