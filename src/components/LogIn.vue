c23f450bbe773a7a375a7e46e775924eab1fb8de
Bilal Khan <bilal.khan@qordata.com>
2022-09-22 22:39:32 +0500

    update LogIn changed container to card

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -1,98 +1,106 @@
 <template>
     <div>
         <v-app>
-            <v-container>
-                <v-row style="margin-top:15%;">
-                    <v-col></v-col>
-                    <v-col>
-                        <v-row>
-                            <v-col cols="4"><img src="./..\assets\Qordata Watermark.png" class="logo newline" /></v-col>
-                            <v-col style="margin-top: 10%;">
-                                <h2><em>Allocations Sheet!</em></h2>
+            <v-row></v-row>
+            <v-row>
+                <v-col></v-col>
+                <v-col>
+                    <v-card shaped class="elevation-5">
+                        <v-row style="margin-top:15%; padding: 20px;">
+                            <!--<v-col></v-col>-->
+                            <v-col>
+                                <v-row>
+                                    <v-col cols="4"><img src="./..\assets\Qordata Watermark.png" class="logo newline" /></v-col>
+                                    <v-col style="margin-top: 10%;">
+                                        <h2>Allocations Sheet!</h2>
+                                    </v-col>
+                                </v-row>
+                                <v-form ref="form"
+                                        v-model="valid"
+                                        lazy-validation>
+                                    <v-text-field v-model="password"
+                                                  :counter="30"
+                                                  :rules="passwordRules"
+                                                  label="Password"
+                                                  :append-icon="value ? 'mdi-eye' : 'mdi-eye-off'"
+                                                  @click:append="() => (value = !value)"
+                                                  :type="value ? 'password' : 'text'"
+                                                  required></v-text-field>
+
+                                    <v-text-field v-model="email"
+                                                  :rules="emailRules"
+                                                  label="E-mail"
+                                                  required></v-text-field>
+
+                                    <v-tooltip bottom>
+                                        <template v-slot:activator="{ on, attrs }">
+                                            <v-btn :disabled="!valid"
+                                                   color="primary"
+                                                   v-bind="attrs"
+                                                   v-on="on"
+                                                   @click="validate(false)">
+                                                Login
+                                            </v-btn>
+                                        </template>
+                                        <span>With great powers, comes great responsibility</span>
+                                    </v-tooltip>
+
+                                    <v-dialog transition="dialog-top-transition"
+                                              max-width="600">
+                                        <template v-slot:activator="{ on, attrs }">
+                                            <a v-bind="attrs" v-on="on" style="font-size: small; margin-left: 10%;">New to Allocation Sheets?</a>
+                                        </template>
+                                        <template v-slot:default="dialog">
+                                            <v-card>
+                                                <v-toolbar color="#8B008B" dark>Create your ID</v-toolbar>
+                                                <v-card-text>
+                                                    <h2 class="newline">Welcome to the team!</h2>
+                                                    <v-row>
+                                                        <v-form ref="form"
+                                                                v-model="new_valid"
+                                                                lazy-validation
+                                                                class="newline"
+                                                                style="margin-left:2%;">
+                                                            <v-text-field v-model="password"
+                                                                          :counter="30"
+                                                                          :rules="passwordRules"
+                                                                          label="Password"
+                                                                          :append-icon="value ? 'mdi-eye' : 'mdi-eye-off'"
+                                                                          @click:append="() => (value = !value)"
+                                                                          :type="value ? 'password' : 'text'"
+                                                                          required>
+                                                            </v-text-field>
+
+                                                            <v-text-field v-model="email"
+                                                                          :rules="emailRules"
+                                                                          label="E-mail"
+                                                                          required>
+                                                            </v-text-field>
+                                                        </v-form>
+                                                    </v-row>
+                                                </v-card-text>
+                                                <v-card-actions class="justify-end">
+                                                    <v-btn text @click="dialog.value = false">Maybe Later</v-btn>
+                                                    <v-btn text @click="validate(true); dialog.value = false;" :disabled="!new_valid">Sign me up!</v-btn>
+                                                </v-card-actions>
+                                            </v-card>
+                                        </template>
+                                    </v-dialog>
+                                    <v-snackbar v-model="executionSnack" :timeout="5000" :color="snackColor">
+                                        {{ snackText }}
+                                        <template v-slot:action="{ attrs }">
+                                            <v-btn v-bind="attrs" text @click="executionSnack = false"> Close </v-btn>
+                                        </template>
+                                    </v-snackbar>
+                                </v-form>
                             </v-col>
+                            <!--<v-col></v-col>-->
                         </v-row>
-                        <v-form ref="form"
-                                v-model="valid"
-                                lazy-validation>
-                            <v-text-field v-model="password"
-                                          :counter="30"
-                                          :rules="passwordRules"
-                                          label="Password"
-                                          :append-icon="value ? 'mdi-eye' : 'mdi-eye-off'"
-                                          @click:append="() => (value = !value)"
-                                          :type="value ? 'password' : 'text'"
-                                          required></v-text-field>
-
-                            <v-text-field v-model="email"
-                                          :rules="emailRules"
-                                          label="E-mail"
-                                          required></v-text-field>
-
-                            <v-tooltip bottom>
-                                <template v-slot:activator="{ on, attrs }">
-                                    <v-btn :disabled="!valid"
-                                           color="primary"
-                                           v-bind="attrs"
-                                           v-on="on"
-                                           @click="validate(false)">
-                                        Login
-                                    </v-btn>
-                                </template>
-                                <span>With great powers, comes great responsibility</span>
-                            </v-tooltip>
-
-                            <v-dialog transition="dialog-top-transition"
-                                      max-width="600">
-                                <template v-slot:activator="{ on, attrs }">
-                                    <a v-bind="attrs" v-on="on" style="font-size: small; margin-left: 10%;">New to Allocation Sheets?</a>
-                                </template>
-                                <template v-slot:default="dialog">
-                                    <v-card>
-                                        <v-toolbar color="#8B008B" dark>Create your ID</v-toolbar>
-                                        <v-card-text>
-                                            <h2 class="newline">Welcome to the team!</h2>
-                                            <v-row>
-                                                <v-form ref="form"
-                                                        v-model="new_valid"
-                                                        lazy-validation
-                                                        class="newline"
-                                                        style="margin-left:2%;">
-                                                    <v-text-field v-model="password"
-                                                                  :counter="30"
-                                                                  :rules="passwordRules"
-                                                                  label="Password"
-                                                                  :append-icon="value ? 'mdi-eye' : 'mdi-eye-off'"
-                                                                  @click:append="() => (value = !value)"
-                                                                  :type="value ? 'password' : 'text'"
-                                                                  required>
-                                                    </v-text-field>
-
-                                                    <v-text-field v-model="email"
-                                                                  :rules="emailRules"
-                                                                  label="E-mail"
-                                                                  required>
-                                                    </v-text-field>
-                                                </v-form>
-                                            </v-row>
-                                        </v-card-text>
-                                        <v-card-actions class="justify-end">
-                                            <v-btn text @click="dialog.value = false">Maybe Later</v-btn>
-                                            <v-btn text @click="validate(true); dialog.value = false;" :disabled="!new_valid">Sign me up!</v-btn>
-                                        </v-card-actions>
-                                    </v-card>
-                                </template>
-                            </v-dialog>
-                            <v-snackbar v-model="executionSnack" :timeout="5000" :color="snackColor">
-                                {{ snackText }}
-                                <template v-slot:action="{ attrs }">
-                                    <v-btn v-bind="attrs" text @click="executionSnack = false"> Close </v-btn>
-                                </template>
-                            </v-snackbar>
-                        </v-form>
-                    </v-col>
-                    <v-col></v-col>
-                </v-row>
-            </v-container>
+                    </v-card>
+                </v-col>
+                <v-col></v-col>
+            </v-row>
+            <v-row></v-row>
         </v-app>
     </div>
 </template>
@@ -142,7 +150,9 @@
                         this.executionSnack = true;
                         //this.$emit('isAuthenticated', true);
                         this.$session.start();
-                        this.$router.push('/hello');
+                        this.$session.set('email', this.email);
+                        this.$session.set('password', this.password);
+                        this.$router.push('/teams');
                     }
                     else if (response.status == 201) {
                         this.snackText = 'Your user has been created. Please login now.';
